d0e9f3907f8dddbb03c7a41157ababc5a48b2a2e
BHOOMIKA <bhoomikaseth999@gmail.com>
2024-10-14 14:19:31 +0530

    Mon, Oct 14, 2024  2:19:31 PM

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,12 +1,12 @@
-# I have created this file
-from django.http import HttpResponse
+# # I have created this file
+# from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+# def index(request):
+#     return HttpResponse("<h1>Hello</h1>")
 
-def about(request):
-    return HttpResponse("Hi")
+# def about(request):
+#     return HttpResponse("Hi")
 
-def abt(request): 
-    file = open("one.txt",'r+')
-    return HttpResponse(file.read()), 
+# def abt(request): 
+#     file = open("one.txt",'r+')
+#     return HttpResponse(file.read()), 
